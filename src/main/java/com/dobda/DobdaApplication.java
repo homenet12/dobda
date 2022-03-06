29793213da94a26016b87e8926a4b016d534d40a
chd <homenet12@naver.com>
2022-03-06 10:54:51 +0900

    dobda start!

---
+++ src/main/java/com/dobda/DobdaApplication.java
@@ -0,0 +1,13 @@
+package com.dobda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobdaApplication.class, args);
+	}
+
+}
