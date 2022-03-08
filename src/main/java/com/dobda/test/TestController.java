e1aa8980b04ed81c449dfb25f74698a55f978358
chd <oom177@naver.com>
2022-03-08 22:22:59 +0900

    cicd test

---
+++ src/main/java/com/dobda/test/TestController.java
@@ -0,0 +1,14 @@
+package com.dobda.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @GetMapping("/test")
+    public String test(){
+        return "hello test!";
+    }
+    //jenkins cicd test!!
+}
