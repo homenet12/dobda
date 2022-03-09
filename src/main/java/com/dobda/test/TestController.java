6970c067a7220b87849ae4749f2d6a4517b408a4
chd <oom177@naver.com>
2022-03-09 18:45:03 +0900

    test

--- src/main/java/com/dobda/test/TestController.java
+++ src/main/java/com/dobda/test/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
     
     @GetMapping("/test")
     public String test(){
-        return "hello test232456g!234234555555";
+        return "hello test";
     }
     //jenkins cicd test!!!
 }
